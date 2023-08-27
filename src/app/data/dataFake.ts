72f042799ad9038b83e01214a607ba719ba33e64
Guilherme <guilhermerblc@gmail.com>
2023-08-27 15:14:29 -0300

    Adding fake data.

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,38 @@
+export const dataFake = [
+    {
+        "cover": "https://image.api.playstation.com/vulcan/ap/rnd/202308/0711/c5d430c1a494773c6de21d2b86fc6716bdded6e3b09f06e3.png",
+        "label": "EDIÇÃO PADRÃO",
+        "type": "PS5 & PS4",
+        "price": "R$ 299,90"
+    },
+    {
+        "cover": "https://static.bandainamcoent.eu/high/tekken/tekken-8/00-page-setup/TEKKEN8_Header_mobile_2.jpg",
+        "label": "EDIÇÃO PADRÃO",
+        "type": "PS5",
+        "price": "R$ 349,90"
+    },
+    {
+        "cover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwy-5_3zZadqbQ3Jw7MC_zUGgWBFn8QO4JkDV_1TecPub1O3SfDwOTJP03sMqfHVizkJ8&usqp=CAU",
+        "label": "EDIÇÃO PADRÃO",
+        "type": "PS5 & PS4",
+        "price": "R$ 299,90"
+    },
+    {
+        "cover": "https://s2-ge.glbimg.com/iA05SKqA_RYQF_xwB-soL9xKQOE=/0x0:1080x1080/600x0/smart/filters:gifv():strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2022/s/0/CidrENTCCVkAkm1qfq7g/sam-kerr-gen5-sk-crop-social-1080x1080.jpg",
+        "label": "Incluído com o EA Play",
+        "type": "PS5",
+        "price": "R$ 338,90"
+    },
+    {
+        "cover": "https://www.adrenaline.com.br/wp-content/plugins/seox-image-magick/imagick_convert.php?width=500&height=500&format=webp&quality=91&imagick=uploads.adrenaline.com.br/2023/08/wayfinder-capa.jpg",
+        "label": "Pacote de Fundador Básico",
+        "type": "PS5 & PS4",
+        "price": "R$ 104,90"
+    },
+    {
+        "cover": "https://www.adrenaline.com.br/wp-content/plugins/seox-image-magick/imagick_convert.php?width=500&height=500&format=webp&quality=91&imagick=uploads.adrenaline.com.br/2023/07/baldurs-gate-3-capa.jpg",
+        "label": "Digital Deluxe Edition",
+        "type": "PS5",
+        "price": "R$ 340,90"
+    }
+]
